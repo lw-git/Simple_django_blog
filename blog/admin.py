531e822addf02ffd4df84e8018f0c742a2620343
lw <lw_git@mail.ru>
2020-03-15 21:16:01 +0500

    Change view in admin panel for post and comment models

--- blog/admin.py
+++ blog/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 from .models import Post, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    list_filter = ('author',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name',
+                    'email',
+                    'author_status',
+                    'post',
+                    'created',
+                    'active')
+    list_filter = ('active', 'author_status', 'created', 'post')
