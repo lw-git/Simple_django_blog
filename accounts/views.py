0cb65e05297a47aa801a43c3204a40be4984d96e
lw <lw_git@mail.ru>
2020-04-28 09:21:31 +0500

    Small changes in signup and login views

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views import generic
+from django.shortcuts import redirect
 
 
 class SignupView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
+
+    def dispatch(self, request, *args, **kwargs):
+
+        if self.request.user.is_authenticated:
+            return redirect(reverse('home'))
+
+        return super().dispatch(request, *args, **kwargs)
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('', include('blog.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
